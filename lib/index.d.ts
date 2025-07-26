224a38fb5865cef22a31a79e20c7fd20635598cf
Akeeb Ismail <damiz.akeeb@gmail.com>
2025-07-26 13:14:30 +0100

    Remove old build files from lib/ directory

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './pubsub.context';
-export * from './pubsub.events';
-export * from './pubsub.server';
-export * from './pubsub.interface';
-export * from './pubsub.client';
-export * from './pubsub.decorator';
--- lib/pubsub.decorator.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const PUBSUB_HANDLER_OPTIONS = "PUBSUB_HANDLER_OPTIONS";
-export interface PubSubHandlerOptions {
-    batch?: boolean;
-    retry?: number;
-}
-export declare function PubSubMessagePattern(pattern: string, options?: PubSubHandlerOptions): (target: any, key: string, descriptor: PropertyDescriptor) => void;
